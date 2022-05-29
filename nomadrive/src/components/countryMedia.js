35a193d653d73ffcf55fb8747b5f55dab13382c5
pedrohdsimoes <63207385+pedrohdsimoes@users.noreply.github.com>
2022-05-29 01:32:14 +0100

    mui material installed; CountryMedia able to pick between image or videos

--- nomadrive/src/components/countryMedia.js
+++ nomadrive/src/components/countryMedia.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import './countryMedia.css';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import Paper from '@mui/material/Paper'
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import Collections from '@mui/icons-material/Collections';
+import VideoLibrary from '@mui/icons-material/VideoLibrary';
 
 export default function CountryMedia() {
+    const [value, setValue] = React.useState(0);
+
     return (
-        <div className="heading">
-            <h1>CountryMedia</h1>
+        <div>
+            <div className="heading">
+                <h1>CountryMedia</h1>
+
+            </div>
+            <div>
+                <Box sx={{ pb: 7 }}>
+                    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+                        <BottomNavigation
+                            showLabels
+                            value={value}
+                            onChange={(event, newValue) => {
+                                setValue(newValue);
+                            }}
+                        >
+                            <BottomNavigationAction label="Photos" icon={<Collections />} />
+                            <BottomNavigationAction label="Videos" icon={<VideoLibrary />} />
+                        </BottomNavigation>
+                    </Paper>
+                </Box>
+            </div>
         </div>
     );
 }
